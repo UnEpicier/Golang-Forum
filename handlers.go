515cae37effb8414b4ee7821697d0c6305adf0ad
UnEpicier <alexisvasseur96@gmail.com>
2022-03-31 10:22:41 +0200

    Error Page

--- handlers.go
+++ handlers.go
@@ -1,11 +1,30 @@
 package forum
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
 
-func ErrorHandler(req http.ResponseWriter, res *http.Request) {
+func ErrorHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		IndexHandler(w, r)
+		return
+	}
 
+	tplt := template.Must(template.ParseFiles("./static/error.html"))
+
+	err := tplt.Execute(w, tplt)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-func IndexHandler(req http.ResponseWriter, res *http.Request) {
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	tplt := template.Must(template.ParseFiles("./static/index.html"))
 
+	err := tplt.Execute(w, interface{}(nil))
+	if err != nil {
+		log.Fatal(err)
+	}
 }
