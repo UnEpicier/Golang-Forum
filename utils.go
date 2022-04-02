eac2a088b217987eededbac8d54ee352f19a2dd2
UnEpicier <alexisvasseur96@gmail.com>
2022-04-02 11:10:35 +0200

    + Hash & Compare functions

--- utils.go
+++ utils.go
@@ -1,5 +1,13 @@
 package forum
 
-func confirmPWD() {
+import "golang.org/x/crypto/bcrypt"
 
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordhash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
